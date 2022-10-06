fa50bf3fe18406333a5be47920040b953f1b0bc6
FazleRabbeBipul <88869686+FazleRabbeBipul@users.noreply.github.com>
2022-10-07 02:10:23 +0600

    Update Floyd-Warshall Algorithm.cpp

--- Graph ALgo/Floyd-Warshall Algorithm.cpp
+++ Graph ALgo/Floyd-Warshall Algorithm.cpp
@@ -1,1 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+const int N = 105;
+int d[N][N];
+int main() {
+  int n = 10;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      if (i != j) {
+        d[i][j] = 1e9;
+      }
+    }
+  }
+
+  ///Case01: if negative weight
+  for (int k = 0; k < n; ++k) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (d[i][k] < INF && d[k][j] < INF)
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+        }
+    }
+  }
+
+  ///Case02: The case of real weights
+  if (d[i][k] + d[k][j] < d[i][j] - EPS)
+    d[i][j] = d[i][k] + d[k][j];
+
+
+
+}
