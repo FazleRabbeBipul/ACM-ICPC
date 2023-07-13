787fabb8c9a8bb3c9d27a8e134831ea975235014
FazleRabbeBipul <88869686+FazleRabbeBipul@users.noreply.github.com>
2023-07-13 22:05:14 +0600

    Add files via upload

---
+++ Segment Tree-Build and Query.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define pii pair<int, int>
+#define pll pair<long long int, long long int>
+#define ALL(s) (s).begin(), (s).end()
+#define rALL(s) (s).rbegin(), (s).rend()
+#define show(x) cout << #x << " : " << x << endl
+
+template <class T>
+using indexed_set =
+    tree<T, null_type, less<T>, rb_tree_tag,
+         tree_order_statistics_node_update>;
+// indexed_set<ll> st;st.order_of_key(x);
+
+const long long mod = 1e9 + 7;
+
+int dx[] = {0, 0, +1, -1};
+int dy[] = {+1, -1, 0, 0};
+
+#define ys cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void build(ll ind, ll low, ll high, vector<ll> &v, vector<ll> &segTree)
+{
+    if (low == high)
+    {
+        segTree[ind] = v[low];
+        return;
+    }
+
+    ll mid = (low + high) / 2;
+
+    build(2 * ind + 1, low, mid, v, segTree);
+    build(2 * ind + 2, mid + 1, high, v, segTree);
+    segTree[ind] = max(segTree[2 * ind + 1], segTree[2 * ind + 2]); // max of left, Right
+}
+
+ll query(ll ind, ll low, ll high, ll l, ll r, vector<ll> &segTree)
+{
+    if (r < low || l > high)
+        return -1;
+    if (low >= l && high <= r) // range completly inside of L-R
+    {
+        return segTree[ind];
+    }
+
+    ll mid = (low + high) / 2;
+    ll left = query(2 * ind + 1, low, mid, l, r, segTree);
+    ll right = query(2 * ind + 2, mid + 1, high, l, r, segTree);
+
+    return max(left, right);
+}
+
+void solve(int tc)
+{
+    // cout << "Case " << tc << ": ";
+    ll n;
+    cin >> n;
+
+    vector<ll> v(n), segTree(4 * n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    build(0, 0, n - 1, v, segTree);
+
+    ll q;
+    cin >> q;
+
+    while (q--)
+    {
+        ll l, r;
+        cin >> l >> r;
+        ll mxValue = query(0, 0, n - 1, l - 1, r - 1, segTree);
+
+        cout << mxValue << endl;
+    }
+}
+
+int main()
+{
+    int tc = 1;
+    // cin >> tc;
+    for (int i = 1; i <= tc; i++)
+        solve(i);
+}
